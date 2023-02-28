ffb94293d6d18b6f6c974eb0043c36ecdf422fb0
mitra-sh <mshahabadi207@gmail.com>
2023-02-28 15:35:56 -0500

    the base structure for project along with H2 configuration

---
+++ src/test/java/com/softwareEngineering/Freelancer/platform/FreelancerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareEngineering.Freelancer.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
